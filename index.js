0565b18d3b464aa3f63e24715395ead5e558fcef
LukaszPudlewski <98075284+LukaszPudlewski@users.noreply.github.com>
2022-06-23 12:08:09 +0100

    a

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoute = require('./routes/user')
+const authRoute = require('./routes/auth')
 
-app.listen(5000, () => {
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO_URL)
+.then(() => console.log('DB Connection success'))
+.catch((err)=>{console.log(err);
+});
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
     console.log("server is running");
 });
