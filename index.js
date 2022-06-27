8207cfbcf1ada5f6a86e852198ae26c82102a075
LukaszPudlewski <98075284+LukaszPudlewski@users.noreply.github.com>
2022-06-27 10:52:54 +0100

    a

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const userRoute = require('./routes/user')
-const authRoute = require('./routes/auth')
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/user');
+const cartRoute = require("./routes/cart");
+const productRoute = require("./routes/product");
+const orderRoute = require("./routes/order");
 
 dotenv.config();
 
@@ -13,9 +16,13 @@ mongoose
 .catch((err)=>{console.log(err);
 });
 
+
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("server is running");
